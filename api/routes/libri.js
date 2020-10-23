f9e588a6c974c2eb5548f3c8cf3473263f0b33ee
matteo <matteoa1999@gmail.com>
2020-10-23 14:10:29 +0200

    add API

--- api/routes/libri.js
+++ api/routes/libri.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const DB = require("../../db/main");
 const JOut = require("../../shared/jout"); // Formatta rispota 
+//const verifyToken= require("../check-auth.js")
 
 router.get("/getAll", (_req, _res, _next) => {
     DB.query({
@@ -13,6 +14,29 @@ router.get("/getAll", (_req, _res, _next) => {
         } else { return _res.status(200).json(JOut(_result, {})); }
     });
 });
+
+
+router.get('/getLibri', (req, res) => {  
+    jwt.verify(req.header.token, 'top-secret', (err, authData) => {
+      if(err) {
+        res.sendStatus(403);
+        console.log("Non valido")
+      } else {/*
+        res.json({
+          message: 'Post created...',
+          authData
+        });*/
+        DB.query({
+            sql:'call getAllLibri()'
+            }, (_err, _result) => {
+            if (_err) {
+                console.log(_err);
+                return _res.status(500).json(JOut([], {}));
+            } else { return _res.status(200).json(JOut(_result, {})); }
+        });
+      }
+    });
+  });
 
 router.get("/getAllKinds", (_req, _res, _next) => {
     DB.query({
@@ -36,4 +60,25 @@ router.post('/addGenere',(_req, _res) => {
     });
   });
 
+/*
+  // Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+      // Split at the space
+      const bearer = bearerHeader.split(' ');
+      // Get token from array
+      const bearerToken = bearer[1];
+      // Set the token
+      req.token = bearerToken;
+      // Next middleware
+      next();
+    } else {
+      // Forbidden
+      res.sendStatus(403);
+    }
+  
+  }*/
 module.exports = router;
