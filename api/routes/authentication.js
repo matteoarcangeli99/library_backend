0936c824b6e9e77f052f58e0b84df92c1299239c
matteo <matteoa1999@gmail.com>
2020-10-27 16:05:15 +0100

    Inserita funzione

--- api/routes/authentication.js
+++ api/routes/authentication.js
@@ -4,6 +4,37 @@ const DB = require("../../db/main");
 const JOut = require("../../shared/jout"); // Formatta rispota 
 const jwt = require("jsonwebtoken");
 
+function cehckAuth(err,result) {
+    if (err) { 
+        return _res.status(401).json(JOut({
+            statusCode: "400",
+            message: "Login failed"
+        }, {}));
+      }  
+    if (result[0].length === 0) { 
+        return _res.status(401).json(JOut({
+            statusCode: "401",
+            message: "Login failed"
+        }, {}));
+    } else {
+        result[0].jwt = jwt.sign({
+            email: result[0].email,
+            id: result[0].ID
+       }, "top_secret", { "expiresIn": "24h"});
+       
+        // REMOVE FIELD
+        delete result[0].password;
+    }
+}
+
+function manageErrore(_res)
+{
+    if (_err) {
+        console.log(_err);
+        return _res.status(500).json(JOut([], {}));
+    } else { return _res.status(201).json(JOut([], {})); }
+}
+
 /**
  * Esegue il login di un utente
  */
@@ -12,27 +43,8 @@ const jwt = require("jsonwebtoken");
         sql: "call userLogin(?,?)",
         values: [_req.body.email, _req.body.password]
     }, (err, result) => {
-        if (err) { 
-            return _res.status(401).json(JOut({
-                statusCode: "400",
-                message: "Login failed"
-            }, {}));
-          }  
-        if (result[0].length === 0) { 
-            return _res.status(401).json(JOut({
-                statusCode: "401",
-                message: "Login failed"
-            }, {}));
-        } else {
-            result[0].jwt = jwt.sign({
-                email: result[0].email,
-                id: result[0].ID
-		   }, "top_secret", { "expiresIn": "24h"});
-		   
-            // REMOVE FIELD
-            delete result[0].password;
-			return _res.status(200).json(JOut(result[0], {}));
-        }
+        cehckAuth(err, result);
+        return _res.status(200).json(JOut(result[0], {}));
     });
 });
 
