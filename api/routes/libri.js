8ed739816f55272939f54db68319dc8df4ad3966
matteo <matteoa1999@gmail.com>
2020-10-30 19:11:51 +0100

    Aggiunta funzionalità

--- api/routes/libri.js
+++ api/routes/libri.js
@@ -18,6 +18,20 @@ router.get("/getAll", (_req, _res, _next) => {
 });
 
 /**
+ * Ritorna un libro dato un ID
+ */
+router.get("/getBook/:id", (_req, _res, _next) => {
+    DB.query({
+        sql:'call getBook(?)', values:[_req.params.id]
+        }, (_err, _result) => {
+        if (_err) {
+            console.log(_err);
+            return _res.status(500).json(JOut([], {}));
+        } else { return _res.status(200).json(JOut(_result, {})); }
+    });
+});
+
+/**
  * ritorna gli autori di un libro
  */
 router.get('/getBookAuthors/:libro',(_req, _res) => {
