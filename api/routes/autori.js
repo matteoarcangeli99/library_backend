c1b22f940b32949fff67759092bcf9179db7e9ff
Matteo <matteoa1999@gmail.com>
2021-02-28 20:05:26 +0100

    autori/getAll

--- api/routes/autori.js
+++ api/routes/autori.js
@@ -3,6 +3,21 @@ const router = express.Router();
 const DB = require("../../db/main");
 const JOut = require("../../shared/jout"); // Formatta rispota 
 
+
+/**
+ * Ritorna tutti gli autori
+ */
+router.get('/getAll', (_req, _res, _next) => {
+    DB.query({
+        sql: 'call getAllAutori',
+    }, (_err, _result) => {
+        if (_err) {
+            return _res.status(500).json(JOut(_err, {}));
+        } else {
+            return _res.status(200).json(JOut(_result, {}));
+        }
+    });
+});
 /**
  * Aggiunge un autore nel
  */
